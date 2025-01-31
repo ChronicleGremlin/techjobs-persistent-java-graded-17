bd5eb106202759a9343cbf482c1e758cdcef5c75
Alec <alecjost7@gmail.com>
2025-01-30 20:33:24 -0600

    added empolyer&skill classes  extend from AbstractEntity, created repositories, and skill controller

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,36 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    // mapping?
+//    @OneToMany(mappedBy = "employer")
+//    private final List<Job> jobs = new ArrayList<>();
+
+    @NotBlank(message = "Field must have one valid name entered")
+    @Size(min = 3, max = 50, message = "Field must be between 3 and 50 characters")
+    private String location;
+
+    public Employer(@NotBlank(message = "Field must have one valid name entered")
+                    @Size(min = 3, max = 50, message = "Field must be between 3 and 50 characters") String location) {
+        this.location = location;
+    }
+
+    public Employer () {};
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,29 @@
 package org.launchcode.techjobs.persistent.models;
 
+
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotBlank(message = "Field must have valid name entered")
+    @Size(min = 3, max = 100, message = "Field must be between 3 and 100 characters")
+    private String description;
+
+    public Skill (@NotBlank(message = "Field must have valid name entered")
+                 @Size(min = 3, max = 50, message = "Field must be between 3 and 100 characters") String description) {
+        this.description = description;
+    }
+
+    public Skill () {}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
