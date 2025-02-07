8f88249d3dc5092646390a4199596a4c3de08c82
Alec <alecjost7@gmail.com>
2025-02-06 21:31:06 -0600

    Updated Employer, Skill and Home Controller to add details to added Jobs

--- queries.sql
+++ queries.sql
@@ -1,12 +1,17 @@
 --Part 1
 --
 -- job table:
+ SELECT *
+ FROM job;
    -- id int
    -- employer varchar
    -- name varchar
    -- skills varchar
 
 --Part 2
+ SELECT name
+ FROM employer
+ WHERE location = "St. Louis City";
 
 --Part 3
 
